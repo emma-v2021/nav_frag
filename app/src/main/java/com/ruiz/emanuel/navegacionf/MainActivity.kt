2da59a67a3e2db8102c3f03ed637a1584cc03f34
Emmanuel Ruiz <emmanuel.ruiz0221@alumnos.udg.mx>
2022-05-27 13:09:05 -0500

    Initial commit

---
+++ app/src/main/java/com/ruiz/emanuel/navegacionf/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ruiz.emanuel.navegacionf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ruiz.emanuel.navegacionf.R
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/ruiz/emanuel/navegacionf/SegundoFragment.kt
@@ -0,0 +1,27 @@
+package com.ruiz.emanuel.navegacionf
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class SegundoFragment : Fragment() {
+    lateinit var btnB: Button
+    lateinit var btnF: Button
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val vistaB= inflater.inflate(R.layout.fragment_segundo, container, false)
+        btnB = vistaB.findViewById(R.id.btn_B)
+        btnB.setOnClickListener {
+            findNavController().navigate(R.id.action_segundoFragment_to_primerFragment)
+        }
+
+        return vistaB
+    }
+}
